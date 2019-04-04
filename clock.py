0c28224af942b6ce9fd2277558eafe25ade9cbeb
tlovestatus <48553699+tlovestatus@users.noreply.github.com>
2019-04-04 10:48:01 +0500

    Update clock.py

--- clock.py
+++ clock.py
@@ -9,12 +9,6 @@ import random
 sys.path.append(os.path.join(sys.path[0], '../'))
 from instabot import Bot
 
-sched = BlockingScheduler()
-
-@sched.scheduled_job('cron', hour=os.environ['hour'],minute=os.environ['minute'])
-def scheduled_job():
-    bot = Bot()
-    bot.login(username=os.environ['username'], password=os.environ['password'])
-    bot.unfollow_everyone()
-
-sched.start()
+bot = Bot()
+bot.login(username=os.environ['username'], password=os.environ['password'])
+bot.unfollow_everyone()
